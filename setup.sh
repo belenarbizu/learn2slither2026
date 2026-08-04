9c90eeee803f76963bfa13ec22c8ec01bb2f9549
barbizu <belenarbizu98@gmail.com>
2026-08-04 17:58:42 +0200

    main init

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Nombre del entorno virtual
+ENV_NAME="env"
+FILE_NAME="requirements.txt"
+
+echo "Creando el entorno virtual '$ENV_NAME'..."
+python3 -m venv "$ENV_NAME"
+
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo crear el entorno virtual."
+fi
+
+echo "Activando el entorno virtual..."
+source "$ENV_NAME"/bin/activate
+
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo activar el entorno virtual."
+    exit 1
+fi
+
+echo "Instalando paquetes..."
+pip install -r "$FILE_NAME"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Falló la instalación de uno o más paquetes."
+    deactivate
+    exit 1
+fi
+
+echo ""
+echo "Instalación completada con éxito en el entorno '$ENV_NAME'."
+echo "Para activar este entorno, ejecuta:"
+echo "source ./$ENV_NAME/bin/activate"
+echo ""
+echo "Para desactivarlo cuando hayas terminado, ejecuta:"
+echo "deactivate"
